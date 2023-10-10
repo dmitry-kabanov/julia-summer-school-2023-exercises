be9b44cc1b2a5428395d28c20178f1ade620a857
Dmitry Kabanov <kabanov.dmitry@gmail.com>
2023-10-10 10:25:05 +0200

    Initial commit

---
+++ src/MyStatsPackage.jl
@@ -0,0 +1,7 @@
+module MyStatsPackage
+
+include("statistic_functions.jl")
+
+export rse_sum, rse_mean, rse_std, rse_tstat
+
+end # module
---
+++ src/statistic_functions.jl
@@ -0,0 +1,32 @@
+using ProgressMeter
+#---
+function rse_sum(arr)
+    s = 0
+    @showprogress for i = 1:length(arr)
+        s += arr[i]
+    end
+
+    return s
+end
+## Test case: rse_sum(1:36) == 666
+#---
+function rse_mean(arr)
+    if length(arr) == 0
+        return 0
+    end
+    s = rse_sum(arr)
+    return s / length(arr)
+end
+
+#---
+function rse_std(arr)
+    mean = rse_mean(arr)
+    return sqrt(+((arr .- mean) .^ 2...) / (length(arr) - 1))
+end
+## Test case rse_std(1:3) == 1
+#---
+
+function rse_tstat(arr; σ=rse_std(arr))
+    return rse_mean(arr) / (σ / sqrt(length(arr)))
+end
+## Test case: rse_tstat(2:3) == 5
